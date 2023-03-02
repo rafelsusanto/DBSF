d0159873c9bcd3d75e00dee379934e4fcc39e0ae
unknown <rafel.susanto@binus.ac.id>
2023-03-02 15:38:45 +0700

    update 02/03/2023

---
+++ DBSF/resources/views/home.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>DBSF</title>
+
+        <!-- CSS -->
+        <link rel="stylesheet" type="text/css" href="{{ asset('css/home.css') }}">
+
+
+    </head>
+    <body>
+        
+    </body>
+</html>
