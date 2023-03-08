b52159263cd4ce51b422e38c81c1850ed7b26850
unknown <rafel.susanto@binus.ac.id>
2023-03-09 02:48:07 +0700

    navbar + footer update

--- DBSF/src/personal/views.py
+++ DBSF/src/personal/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 
 # Create your views here.
 def home_screen_view(request):
-    print(request.headers)
+    # print(request.headers)
     return render(request, "home.html",{})
