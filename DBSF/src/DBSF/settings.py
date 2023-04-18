b4f27cb7469076d907c56bbe40904e0785d0ef67
Squid Squid <ida.dipa@binus.ac.id>
2023-04-19 01:10:14 +0700

    added config for database & sql file on src

--- DBSF/src/DBSF/settings.py
+++ DBSF/src/DBSF/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'DBSF.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'dbsf',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+        'USER': 'django',
+        'PASSWORD': 'django-user-password',
     }
 }
 
