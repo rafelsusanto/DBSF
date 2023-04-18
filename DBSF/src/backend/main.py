3ffb9a06ffba462fdc3eaba313cd4ea2282a9a0e
Squid Squid <ida.dipa@binus.ac.id>
2023-04-18 19:30:38 +0700

    successful test sqlmap run through subprocess

--- DBSF/src/backend/main.py
+++ DBSF/src/backend/main.py
@@ -1,10 +1,14 @@
 import os
 import subprocess
 import sqlmap
+import sys
 
+def runsqlmap(command):
+    subprocess.run(command, shell=True)
 
-def testprint():
-    print("hello world")
+#def testprint():
+    #print("hello world")
 
+runstring = "sqlmap -h"
 
-testprint()
+runsqlmap(runstring)
