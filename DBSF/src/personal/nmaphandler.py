a52b3b63bc7807efa9fabe15e8744250d7a8dae5
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 <rafel.susanto@binus.ac.id>
2023-05-17 14:24:02 +0700

    update

--- DBSF/src/personal/nmaphandler.py
+++ DBSF/src/personal/nmaphandler.py
@@ -7,26 +7,50 @@ from .models import *
 from .forms import *
 
 def run_thread(ip,db_id):
-    thread = threading.Thread(target=run_nmap,args=(ip,db_id,))
+    thread = threading.Thread(target=run_script,args=(ip,db_id,))
     thread.setDaemon(True)
     thread.start()
 
 def run_nmap(ip,db_id):
-    CMD = "nmap -p 80 "+ ip
+    CMD = "nmap -sC -sV "+ ip
     op = subprocess.run(CMD, shell=True, stdout=subprocess.PIPE)
     hasil = op.stdout
     hasil = hasil.decode('UTF-8')
 
-    form = ScanResultForm({'ScanID': db_id, 'ScanType' : "1", 'Description': hasil})
-    form.save()
+    # Checking if host is up
+    if hasil.find("Host is up"):
+        container = hasil.split('\n')
+        db_type = 0
+        # check if port is mysql / oracle 
+        result = ""
+        for c in container:
+            if c.find('mysql')!=-1 or c.find('oracle')!=-1:
+                result += c
+                result += '\n' 
+
+                # jalanin threading buat hydra scanning
+                
+        
+        print(result)
+        form = ScanResultForm({'ScanID': db_id, 'ScanType' : "1", 'Description': hasil})
+        form.save()
+    else:
+        UPDATE = Scan.objects.get(pk=db_id)
+        UPDATE.Status = "Failed"
+        UPDATE.save()
+
+        form = ScanResultForm({'ScanID': db_id, 'ScanType' : "9", 'Description': "Fail to run nmap!"})
+        form.save()
+    
+
+def run_script(ip,db_id):
+    # run nmap
+    run_nmap(ip,db_id)
 
     UPDATE = Scan.objects.get(pk=db_id)
     UPDATE.Status = "Done"
     UPDATE.save()
 
-
-
-    print(hasil)
 
 def test_ls():
     cmd = "ls"
