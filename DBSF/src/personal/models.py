699c1a8e988417b0f8581bcebdefe36cb96da358
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 <rafel.susanto@binus.ac.id>
2023-05-29 18:19:46 +0700

    update scanlist frontend

--- DBSF/src/personal/models.py
+++ DBSF/src/personal/models.py
@@ -6,6 +6,7 @@ class Scan(models.Model):
     IPAddress = models.CharField(max_length=20)
     Status = models.CharField(max_length=8, default="On Going")
     PacketRequest = models.FileField(blank=True, null=True, upload_to='pr/')
+    Created_at = models.DateTimeField(auto_now_add=True)
 
 class ScanResult(models.Model):
     ScanID = models.ForeignKey(Scan, on_delete=models.CASCADE)
