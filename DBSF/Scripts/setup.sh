5c91ca6c486c28e65e50be9965436633a64fd423
Squid Squid <ida.dipa@binus.ac.id>
2023-06-05 13:14:47 +0700

    fixed mysql install

--- DBSF/Scripts/setup.sh
+++ DBSF/Scripts/setup.sh
@@ -6,7 +6,8 @@ sudo apt-get install -y nmap
 sudo apt-get install -y sqlmap
 sudo apt-get install -y hydra
 sudo apt-get install -y python3-scapy
-sudo pip3 install colorlog termcolor pycrypto passlib python-libnmap libaio1 python3-dev alien python3-pip cx_Oracle
+sudo apt-get install -y default-libmysqlclient-dev
+sudo pip3 install colorlog termcolor pycrypto passlib python-libnmap python3-dev alien python3-pip cx_Oracle
 sudo pip3 install argcomplete && sudo activate-global-python-argcomplete
-
 python3 -m pip install -r ../src/requirements.txt
+git submodule update --init --recursive
