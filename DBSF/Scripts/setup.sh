22d79e7dbf66e3055a32ab22ddbfc01fc119fc60
Squid Squid <ida.dipa@binus.ac.id>
2023-06-11 17:18:35 +0700

    added dockerfile updates

--- DBSF/Scripts/setup.sh
+++ DBSF/Scripts/setup.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 
+source ../env/bin/activate
+
 sudo apt-get update -y
 
-sudo apt-get install -y nmap
-sudo apt-get install -y sqlmap
-sudo apt-get install -y hydra
-sudo apt-get install -y python3-scapy
-sudo apt-get install -y odat
-sudo apt-get install -y default-libmysqlclient-dev
-sudo pip3 install colorlog termcolor pycrypto passlib python-libnmap python3-dev alien python3-pip cx_Oracle
+sudo apt-get install -y nmap sqlmap hydra python3-scapy odat default-libmysqlclient-dev
+sudo pip3 install colorlog termcolor pycrypto passlib python-libnmap libpython3-dev alien python3-pip cx_Oracle
 sudo pip3 install argcomplete && sudo activate-global-python-argcomplete
 python3 -m pip install -r ../src/requirements.txt
 git submodule update --init --recursive
--- DBSF/src/DBSF/settings.py
+++ DBSF/src/DBSF/settings.py
@@ -86,7 +86,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'dbsf',
-        'HOST': '127.0.0.1',
+        'HOST': '172.16.238.10',
         'PORT': '3306',
         'USER': 'django',
         'PASSWORD': 'django-user-password',
