a9a9891ef1ad448a476cb1291a865cc7d3763ac2
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 <rafel.susanto@binus.ac.id>
2023-06-11 16:29:21 +0700

    update

--- DBSF/src/static/js/scanlist.js
+++
@@ -1,9 +0,0 @@
-function finalurl(){
-    var url = new URL(window.location.href);
-    var search_params = url.searchParams
-    search_params.set('sort',document.getElementById("sort-list").value);
-    url.search=search_params.toString();
-    var newurl = url.toString()
-    console.log(newurl)
-    window.location.href = newurl;
-}
