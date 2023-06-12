bf8a3d616644305cc8a915361753b6b8baad27d1
Squid Squid <ida.dipa@binus.ac.id>
2023-06-12 14:14:43 +0700

    fixed database configuration

--- DBSF/src/DBSF/settings.py
+++ DBSF/src/DBSF/settings.py
@@ -86,7 +86,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'dbsf',
-        'HOST': 'db',
+        'HOST': '127.0.0.1',
         'PORT': '3306',
         'USER': 'django',
         'PASSWORD': 'django-user-password',
